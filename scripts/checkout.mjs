0d8cb3d9169cd4ac423be1c2e27089bf72eca832
EliNygard <eli.nygard@gmail.com>
2024-04-09 11:54:37 +0200

    creating cart html

--- scripts/checkout.mjs
+++ scripts/checkout.mjs
@@ -32,18 +32,46 @@ function generateEmptyCartMessage() {
 function generateCartHtml(film) {
     const cartItem = document.createElement("div");
     cartItem.classList.add("cart-item")
-
+    
     const linkContainer = document.createElement("a")
+    linkContainer.href = "filmpage.html" 
     
-    
+    const imageElement = document.createElement("img")
+    imageElement.classList.add("cart-image")
+    imageElement.src = film.image.url
+    imageElement.alt = film.image.alt
+
+    const infoContainer = document.createElement("div")
+    infoContainer.classList.add("info-element")
+
+    const titleElement = document.createElement("p")
+    titleElement.classList.add("cart-item-title")
+    titleElement.textContent = film.title
+
+    const priceElement = document.createElement("p")
+    priceElement.classList.add("cart-item-price")
+    if (film.onSale) {
+        priceElement.textContent = `${film.discountedPrice} kr`
+        const priceBefore = document.createElement("p")
+        priceBefore.classList.add("cart-price-before")
+        priceBefore.textContent = `Before ${film.price} kr`
+
+        priceElement.appendChild(priceBefore)
+    }
+
+    cartItem.append(linkContainer, infoContainer)
+    linkContainer.appendChild(imageElement)
+    infoContainer.append(titleElement, priceElement)
+
+    return cartItem;
     }
 
 function displayCartItems() {
-    const cartContainer = document.getElementById("#cart-container");
-    cartContainer.textContent = ''
-    const cart = JSON.parse(localStorage.getItem('cart'))
+    const cartContainer = document.getElementById("cart-container");
+    cartContainer.textContent = '';
+    const cart = JSON.parse(localStorage.getItem("cart"))
 
-    cart.forEach(function (currentItem) {
+    cart.forEach(currentItem => {
         const itemHtml = generateCartHtml(currentItem)
         cartContainer.appendChild(itemHtml)
     })
